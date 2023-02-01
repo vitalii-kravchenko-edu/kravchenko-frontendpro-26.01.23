5d933f6c46ecbe01fe8f84e77331cee0198eef65
Vitalii Kravchenko <opabbk@gmail.com>
2023-02-01 20:44:20 +0200

    dz2 several ways

--- dz2/script.js
+++ dz2/script.js
@@ -1,2 +1,12 @@
+// 1 варіант
 let userName = prompt("What is your name?", '');
 alert(`Hello, ${userName}! How are you?`);
+
+
+// 2 варіант
+// let userName = prompt("What is your name?", '');
+// alert("Hello, "+ userName +"! How are you?");
+
+
+// 3 варіант
+// alert("Hello, " + prompt("What is your name?", '') +"! How are you?");
