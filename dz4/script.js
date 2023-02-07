23640c1b35f32eadeb117e3d33ae940bfba73efe
Vitalii Kravchenko <opabbk@gmail.com>
2023-02-07 19:50:18 +0200

    dz4

---
+++ dz4/script.js
@@ -0,0 +1,23 @@
+const nomber1 = +prompt('Введіть перше число', '');
+const nomber2 = +prompt('Введіть друге число', '');
+
+const resultAdd = nomber1 + nomber2;
+const resultSub = nomber1 - nomber2;
+const resultMult = nomber1 * nomber2;
+const resultDiv = nomber1 / nomber2;
+
+alert (`Користувач ввів ${nomber1} і ${nomber2}:
+    ${nomber1} + ${nomber2} = ${resultAdd}
+    ${nomber1} - ${nomber2} = ${resultSub}
+    ${nomber1} * ${nomber2} = ${resultMult}
+    ${nomber1} / ${nomber2} = ${resultDiv}`);
+
+
+
+
+
+
+
+
+
+
