ed566fa2b771b1804b83aa8989ddb944cd65e7aa
Vitalii Kravchenko <opabbk@gmail.com>
2023-02-07 19:45:53 +0200

    dz3

---
+++ dz3/Part1/script.js
@@ -0,0 +1,17 @@
+const string1 = prompt('Введіть перший рядок', '');
+const string2 = prompt('Введіть другий рядок', '');
+const string3 = prompt('Введіть третій рядок', '');
+
+const result = (string1 + ' ' + string2 + ' ' + string3);
+
+alert (result);
+
+
+
+
+
+
+
+
+
+
