a6bcd6990d859d0d04f704e6fbc0618c26e0028e
Vitalii Kravchenko <opabbk@gmail.com>
2023-02-07 19:55:30 +0200

    dz7

---
+++ dz7/script.js
@@ -0,0 +1,17 @@
+const nomber1 = +prompt('Введіть перше число', '');
+const nomber2 = +prompt('Введіть друге число', '');
+const nomber3 = +prompt('Введіть третє число', '');
+
+const result = (nomber1 + nomber2 + nomber3) / 3;
+
+alert (`Середнє арифметичне чисел ${nomber1}, ${nomber2} та ${nomber3} дорівнює ${result}`);
+
+
+
+
+
+
+
+
+
+
