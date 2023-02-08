315b9256bfc10ff18f8f0e3a694985ae287089b0
Vitalii Kravchenko <opabbk@gmail.com>
2023-02-08 20:37:56 +0200

    dz3 fixed

--- dz3/Part2/script.js
+++ dz3/Part2/script.js
@@ -1,9 +1,10 @@
 const numb = prompt('Введіть п\'ятизначне число', '12345');
 
-alert ('Ваше число складається з цифр: ' + numb[0] + ' ' + numb[1] + ' ' + numb[2] + ' ' + numb[3] + ' ' + numb[4]);
+// alert ('Ваше число складається з цифр: ' + numb[0] + ' ' + numb[1] + ' ' + numb[2] + ' ' + numb[3] + ' ' + numb[4]);
 
 
-
+const result = numb.split('').join(' ');
+alert (result);
 
 
 
